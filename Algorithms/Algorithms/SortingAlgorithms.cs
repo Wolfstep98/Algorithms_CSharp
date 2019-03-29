bf4460fa75cf70582ba6116faaea3d393acfe1c9
Benjamin Peter <benji.peter@hotmail.fr>
2019-03-29 15:13:44 +0100

    Added Shuffle Algorithms
    - Added Shuffle Algorithms

--- Algorithms/Algorithms/SortingAlgorithms.cs
+++ Algorithms/Algorithms/SortingAlgorithms.cs
@@ -1,104 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Algorithms
+namespace Algorithms.Sorting
 {
-    namespace SortingAlgorithms
+    /// <summary>
+    /// Main class for sortings algortihms.
+    /// </summary>
+    /// <typeparam name="T">Type to sort. Must implement IComparable<typeparamref name="T"/> interface.</typeparam>
+    public static class Sort<T> where T : IComparable<T>
     {
-        public static class Sort<T> where T : IComparable<T>
+        /// <summary>
+        /// Sort the array using the Selection Sort Algorithms.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        public static void SelectionSort(T[] array)
         {
-            /// <summary>
-            /// Sort the array using the Selection Sort Algorithms.
-            /// </summary>
-            /// <param name="array">The array to sort.</param>
-            public static void SelectionSort(T[] array)
+            if (array == null)
+                throw new NullReferenceException("The array you try to sort is referenced to null.");
+            if (array.Length == 0 || array.Length == 1)
+                return;
+
+            int minIndex = 0;
+
+            for (int index = 0; index < array.Length - 1; index++)
             {
-                if (array == null)
-                    throw new NullReferenceException("The array you try to sort is referenced to null.");
-                if (array.Length == 0 || array.Length == 1)
-                    return;
-
-                int minIndex = 0;
-
-                for(int index = 0; index < array.Length - 1; index++)
+                minIndex = index;
+                for (int i = index + 1; i < array.Length; i++)
                 {
-                    minIndex = index;
-                    for(int i = index + 1; i < array.Length;i++)
-                    {
-                        if (array[i].CompareTo(array[minIndex]) == -1)
-                            minIndex = i;
-                    }
-                    if (index != minIndex)
-                        Swap(array, index, minIndex);    
+                    if (array[i].CompareTo(array[minIndex]) == -1)
+                        minIndex = i;
                 }
+                if (index != minIndex)
+                    Swap(array, index, minIndex);
             }
+        }
 
-            /// <summary>
-            /// Sort the array using the Insertion Sort Algorithms.
-            /// </summary>
-            /// <param name="array">The array to sort.</param>
-            public static void InsertionSort(T[] array)
+        /// <summary>
+        /// Sort the array using the Insertion Sort Algorithms.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        public static void InsertionSort(T[] array)
+        {
+            if (array == null)
+                throw new NullReferenceException("The array you try to sort is referenced to null.");
+            if (array.Length == 0 || array.Length == 1)
+                return;
+
+            for (int i = 0; i < array.Length; i++)
             {
-                if (array == null)
-                    throw new NullReferenceException("The array you try to sort is referenced to null.");
-                if (array.Length == 0 || array.Length == 1)
-                    return;
-
-                for (int i = 0; i < array.Length; i++)
+                T indexValue = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j].CompareTo(indexValue) == 1)
                 {
-                    T indexValue = array[i];
-                    int j = i - 1;
-                    while (j >= 0 && array[j].CompareTo(indexValue) == 1)
-                    {
-                        array[j + 1] = array[j];
-                        j--;
-                    }
-                    array[j + 1] = indexValue;
+                    array[j + 1] = array[j];
+                    j--;
                 }
+                array[j + 1] = indexValue;
             }
+        }
 
-            /// <summary>
-            /// Sort the array using the Bubble Sort Algorithms.
-            /// </summary>
-            /// <param name="array">The array to sort.</param>
-            public static void BubbleSort(T[] array)
+        /// <summary>
+        /// Sort the array using the Bubble Sort Algorithms.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        public static void BubbleSort(T[] array)
+        {
+            bool swapped = true;
+            while (swapped)
             {
-                bool swapped = true;
-                while(swapped)
+                swapped = false;
+                for (int i = 0; i < array.Length - 1; i++)
                 {
-                    swapped = false;
-                    for(int i = 0; i < array.Length - 1;i++)
+                    if (array[i].CompareTo(array[i + 1]) == 1)
                     {
-                        if(array[i].CompareTo(array[i+1]) == 1)
-                        {
-                            if (!swapped) swapped = true;
-                            Swap(array, i, i + 1);
-                        }
+                        if (!swapped) swapped = true;
+                        Swap(array, i, i + 1);
                     }
                 }
             }
+        }
 
-            /// <summary>
-            /// Sort the array using the Shell Sort Algorithms.
-            /// </summary>
-            /// <param name="array">The array to sort.</param>
-            public static void SheelSort(T[] array)
-            {
+        /// <summary>
+        /// Sort the array using the Shell Sort Algorithms.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        public static void SheelSort(T[] array)
+        {
 
-            }
+        }
 
-            /// <summary>
-            /// Swap 2 values at 2 differents indexes.
-            /// </summary>
-            /// <param name="array">The array to swap values.</param>
-            /// <param name="index">Index of the first value to swap.</param>
-            /// <param name="minIndex">Index of the second value to swap.</param>
-            private static void Swap(T[] array, int index, int minIndex)
-            {
-                T temp = array[index];
-                array[index] = array[minIndex];
-                array[minIndex] = temp;
-            }
+        /// <summary>
+        /// Swap 2 values at 2 differents indexes.
+        /// </summary>
+        /// <param name="array">The array to swap values.</param>
+        /// <param name="index">Index of the first value to swap.</param>
+        /// <param name="minIndex">Index of the second value to swap.</param>
+        private static void Swap(T[] array, int index, int minIndex)
+        {
+            T temp = array[index];
+            array[index] = array[minIndex];
+            array[minIndex] = temp;
         }
     }
 }
